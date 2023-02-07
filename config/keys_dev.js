8fc424135605ce1f127949ce3875c6cd7e1298eb
Misha <misha77b@gmail.com>
2023-02-07 17:50:29 +0200

    deleted keys_dev and added it to gitignore

--- config/keys_dev.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  mongoURI:
-    'mongodb+srv://Admin_fe4:fe4group2@cluster0.ywjxnvu.mongodb.net/?retryWrites=true&w=majority',
-  secretOrKey: 'random very very secret string',
-};
