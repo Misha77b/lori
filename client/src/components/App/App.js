2a359423e6cc4734cb7531f0b0c4041331674c87
Misha <misha77b@gmail.com>
2023-02-08 22:55:53 +0200

    create basic sections like header, main, footer

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
+import Pages from '../Pages/Pages';
 
 const App = () => {
   return (
     <BrowserRouter>
 
       <Header />
-      <div className="App"> App </div>
+      <Pages />
+      <Footer />
       
     </BrowserRouter>
   );
