b04eb97e73a9674276aa581cde00fc67268eb887
Misha <misha77b@gmail.com>
2023-02-08 08:57:50 +0200

    added reduxjsToolkit and tuned provider store

---
+++ client/src/store/rootreducer.js
@@ -0,0 +1,13 @@
+import { combineReducers } from 'redux'; 
+ 
+// import exampleReducer from '..API/example/example';  
+
+
+
+
+const rootReducer = combineReducers({
+    // exampleReducer, 
+}); 
+
+
+export default rootReducer; 
