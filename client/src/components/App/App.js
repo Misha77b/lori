dc4e6261228047ef6c758d0db63c8d28c5442f7c
Vitalina Porada <porada.vita@icloud.com>
2023-02-09 15:12:50 +0200

    added theming and fixed store

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,19 +1,24 @@
-import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import Footer from '../Footer/Footer';
-import Header from '../Header/Header';
-import Pages from '../Pages/Pages';
-
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "@mui/material";
+import { Button, Typography } from "@mui/material";
+import theme from "../../theming";
+import Footer from "../Footer/Footer";
+import Header from "../Header/Header";
+import Pages from "../Pages/Pages";
 const App = () => {
   return (
-    <BrowserRouter>
-
-      <Header />
-      <Pages />
-      <Footer />
-      
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Header />
+        <Button color="secondary" variant="contained">
+          Детальніше
+        </Button>
+        <Pages />
+        <Footer />
+      </BrowserRouter>
+    </ThemeProvider>
   );
-}
+};
 
 export default App;
