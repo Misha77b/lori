0fe5899bd7f0b9466c247306bd9f537d708694c4
Misha <misha77b@gmail.com>
2023-02-09 11:56:27 +0200

    basic routing settings

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,19 +1,9 @@
 import React from 'react';
-import { Button } from '@mui/material';
-import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <div>
       Header
-
-      <Button 
-        component={Link}
-        to='/another-page'
-        variant='outlined'
-      >
-        Another page
-      </Button>
     </div>
   )
 }
--- client/src/components/Pages/Pages.jsx
+++ client/src/components/Pages/Pages.jsx
@@ -2,16 +2,12 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom'; 
 
 import Home from '../Home/Home';
-import TestFileAnotherPage from '../TestFileAnotherPage';
 
 const Pages = () => {
   return (
     <Routes>
 
       <Route path='/' element={ <Home /> } />
-
-      {/* example route */}
-      <Route path='another-page' element={ <TestFileAnotherPage /> } />
 
     </Routes>
   )
