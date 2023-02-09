8e5161ad925ad27a871be578d78cfa717ff5168a
Vitalina Porada <porada.vita@icloud.com>
2023-02-09 15:12:50 +0200

    made global theming

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,15 +1,9 @@
-import { configureStore } from '@reduxjs/toolkit'
-import thunk from 'redux-thunk'; 
-
-import rootReducer from './rootreducer';
-
-const reduxDevToolsCompose = window['__REDUX_DEVTOOLS_EXTENSION_COMPOSE__'];
-
+import { configureStore } from "@reduxjs/toolkit";
+import someReducer from "../reducers/slice";
 const store = configureStore({
-    devTools: reduxDevToolsCompose,
-    reducer: rootReducer,
-    middleware: [thunk],
-})
+  reducer: {
+    some: someReducer,
+  },
+});
 
-
-export default store; 
+export default store;
