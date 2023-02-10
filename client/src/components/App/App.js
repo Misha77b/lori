39b725734fa871be5c7e91ecf942b7d2706c6802
Misha <misha77b@gmail.com>
2023-02-10 14:32:09 +0200

    checking how husky works

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,17 +1,24 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "@mui/material";
+import { Button, Typography } from "@mui/material";
+import theme from "../../theming";
 import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
 import Pages from "../Pages/Pages";
-
-function App() {
-	return (
-		<BrowserRouter>
-			<Header />
-			<Pages />
-			<Footer />
-		</BrowserRouter>
-	);
-}
+const App = () => {
+  return (
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Header />
+        <Button color="secondary" variant="contained">
+          Детальніше
+        </Button>
+        <Pages />
+        <Footer />
+      </BrowserRouter>
+    </ThemeProvider>
+  );
+};
 
 export default App;
