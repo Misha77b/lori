af115e6edf40454c2bcb08b63d35d6f6edff8ec8
Inna <kurylo.salamonova@gmail.com>
2023-02-11 20:58:29 +0200

    husky update

---
+++ client/src/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import PopularBrands from "../../components/PopularBrands";
+
+const Home = () => {
+	return (
+		<>
+			<PopularBrands></PopularBrands>
+		</>
+	);
+};
+export default Home;
