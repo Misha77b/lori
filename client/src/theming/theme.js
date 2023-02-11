42b35ad2e4462f37e56841562f9cae3feffb8896
Misha <misha77b@gmail.com>
2023-02-11 13:58:03 +0200

    husky-check

--- client/src/theming/theme.js
+++ client/src/theming/theme.js
@@ -19,27 +19,15 @@ const theme = createTheme({
 		primary: {
 			main: "#FFFFFF",
 		},
-		grey: { 
-								main: "#57646E" 
-		},
-		lightgrey: { 
-			main: "#D3D7DA" 
-		},
-		mediumgrey: { 
-			main: "#A0A9AF" 
-		},
-		darkgrey: { 
-																main: "rgba(33, 39, 40, 0.6)" 
-		},
+		grey: { main: "#57646E" },
+		lightgrey: { main: "#D3D7DA" },
+		mediumgrey: { main: "#A0A9AF" },
+		darkgrey: { main: "rgba(33, 39, 40, 0.6)" },
 		secondary: {
 			main: colors.green[800],
 		},
-		graphite: { 
-			main: "#2E3438" 
-		},
-		black: { 
-			main: "#000000" 
-		},
+		graphite: { main: "#2E3438" },
+		black: { main: "#000000" },
 		warning: {
 			main: colors.orange[400],
 		},
