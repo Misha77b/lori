0459cbeecd0e35b88e48fec37c0d2f291bb4c7c7
Vitalina Porada <porada.vita@icloud.com>
2023-02-12 21:55:26 +0200

    deleted footer and header

--- client/src/App/App.js
+++ client/src/App/App.js
@@ -3,16 +3,14 @@ import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "@mui/material";
 import RootRouters from "../Router/Router";
 import theme from "../theming";
-import Header from "../components/Header/Header";
-import Footer from "../components/Footer/Footer";
+// import Header from "../components/Header/Header";
+// import Footer from "../components/Footer/Footer";
 
 const App = () => {
 	return (
 		<ThemeProvider theme={theme}>
 			<BrowserRouter>
-				<Header />
 				<RootRouters />
-				<Footer />
 			</BrowserRouter>
 		</ThemeProvider>
 	);
