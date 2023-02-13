c8e42eafe3d56dc7f779136888645bd8fcf7460f
Misha <misha77b@gmail.com>
2023-02-13 18:57:48 +0200

    slider content style

--- client/src/components/Slider/Slider.jsx
+++ client/src/components/Slider/Slider.jsx
@@ -4,8 +4,17 @@ import { Navigation, Pagination } from "swiper";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
-import { Box } from "@mui/material";
+import { Box, Button, Typography } from "@mui/material";
 import "./Slider.scss";
+import {
+	slideTextContentStyle,
+	lightSlideTitle,
+	darkSlideTitle,
+	lightSlideDescription,
+	darkSlideDescription,
+	darkBackButton,
+	lightBackButton,
+} from "./sxMuiComponents/style";
 
 const Slider = () => {
 	return (
@@ -13,6 +22,7 @@ const Slider = () => {
 			<Swiper
 				navigation={true}
 				slidesPerView={1}
+				speed={600}
 				spaceBetween={30}
 				pagination={{
 					clickable: true,
@@ -21,32 +31,167 @@ const Slider = () => {
 				className="mySwiper"
 			>
 				<SwiperSlide className="swiper-item">
-					<img
-						className="swiper-slide-img"
-						style={{ width: "100%", height: "100%" }}
-						src="https://res.cloudinary.com/dsx708og4/image/upload/v1676222627/Lori_project/Slide1_iPhone-14-pro-max_paqdb6.jpg"
-					/>
+					<Box
+						component="div"
+						className="slide-box"
+					>
+						<img
+							className="swiper-slide-img"
+							style={{ width: "100%", height: "100%" }}
+							src="https://res.cloudinary.com/dsx708og4/image/upload/v1676222627/Lori_project/Slide1_iPhone-14-pro-max_paqdb6.jpg"
+						/>
+						<Box
+							className="slide-text-content"
+							sx={slideTextContentStyle}
+						>
+							<Typography
+								variant="h3"
+								fontWeight="fontWeightBold"
+								sx={lightSlideTitle}
+								className="slide-ingo-title"
+							>
+								iPhone 14 Pro Max
+							</Typography>
+
+							<Typography
+								sx={lightSlideDescription}
+								className="slide-info-descr"
+							>
+								Не пропустіть нашу гарячу пропозицію!
+							</Typography>
+
+							<Button
+								color="primary"
+								variant="contained"
+								sx={darkBackButton}
+							>
+								Детальніше
+							</Button>
+						</Box>
+					</Box>
 				</SwiperSlide>
+
 				<SwiperSlide className="swiper-item">
-					<img
-						className="swiper-slide-img"
-						style={{ width: "100%", height: "100%" }}
-						src="https://res.cloudinary.com/dsx708og4/image/upload/v1676222653/Lori_project/Slide2_Samsung-s23-ultra_nlgwjf.jpg"
-					/>
+					<Box
+						component="div"
+						className="slide-box"
+					>
+						<img
+							className="swiper-slide-img"
+							style={{ width: "100%", height: "100%" }}
+							src="https://res.cloudinary.com/dsx708og4/image/upload/v1676222653/Lori_project/Slide2_Samsung-s23-ultra_nlgwjf.jpg"
+						/>
+						<Box
+							className="slide-text-content"
+							sx={slideTextContentStyle}
+						>
+							<Typography
+								variant="h3"
+								fontWeight="fontWeightBold"
+								sx={lightSlideTitle}
+								className="slide-ingo-title"
+							>
+								Samsung S23 Ultra
+							</Typography>
+
+							<Typography
+								sx={lightSlideDescription}
+								className="slide-info-descr"
+							>
+								Не пропустіть нашу гарячу пропозицію!
+							</Typography>
+
+							<Button
+								color="primary"
+								variant="contained"
+								sx={darkBackButton}
+							>
+								Детальніше
+							</Button>
+						</Box>
+					</Box>
 				</SwiperSlide>
+
 				<SwiperSlide className="swiper-item">
-					<img
-						className="swiper-slide-img"
-						style={{ width: "100%", height: "100%" }}
-						src="https://res.cloudinary.com/dsx708og4/image/upload/v1676222787/Lori_project/slide3_Huawei-mate-50-series_whoidn.jpg"
-					/>
+					<Box
+						component="div"
+						className="slide-box"
+					>
+						<img
+							className="swiper-slide-img"
+							style={{ width: "100%", height: "100%" }}
+							src="https://res.cloudinary.com/dsx708og4/image/upload/v1676222787/Lori_project/slide3_Huawei-mate-50-series_whoidn.jpg"
+						/>
+						<Box
+							className="slide-text-content"
+							sx={slideTextContentStyle}
+						>
+							<Typography
+								variant="h3"
+								fontWeight="fontWeightBold"
+								sx={darkSlideTitle}
+								className="slide-ingo-title"
+							>
+								Huawei Mate 50 Series
+							</Typography>
+
+							<Typography
+								sx={darkSlideDescription}
+								className="slide-info-descr"
+							>
+								Не пропустіть нашу гарячу пропозицію!
+							</Typography>
+
+							<Button
+								color="secondary"
+								variant="contained"
+								sx={lightBackButton}
+							>
+								Детальніше
+							</Button>
+						</Box>
+					</Box>
 				</SwiperSlide>
+
 				<SwiperSlide className="swiper-item">
-					<img
-						className="swiper-slide-img"
-						style={{ width: "100%", height: "100%" }}
-						src="https://res.cloudinary.com/dsx708og4/image/upload/v1676223534/Lori_project/Slide4_Xiaomi-11-lite_d23daa.jpg"
-					/>
+					<Box
+						component="div"
+						className="slide-box"
+					>
+						<img
+							className="swiper-slide-img"
+							style={{ width: "100%", height: "100%" }}
+							src="https://res.cloudinary.com/dsx708og4/image/upload/v1676223534/Lori_project/Slide4_Xiaomi-11-lite_d23daa.jpg"
+						/>
+						<Box
+							className="slide-text-content"
+							sx={slideTextContentStyle}
+						>
+							<Typography
+								variant="h3"
+								fontWeight="fontWeightBold"
+								sx={darkSlideTitle}
+								className="slide-ingo-title"
+							>
+								Xiaomi 11 Lite
+							</Typography>
+
+							<Typography
+								sx={darkSlideDescription}
+								className="slide-info-descr"
+							>
+								Не пропустіть нашу гарячу пропозицію!
+							</Typography>
+
+							<Button
+								color="secondary"
+								variant="contained"
+								sx={lightBackButton}
+							>
+								Детальніше
+							</Button>
+						</Box>
+					</Box>
 				</SwiperSlide>
 			</Swiper>
 		</Box>
---
+++ client/src/components/Slider/sxMuiComponents/style.js
@@ -0,0 +1,39 @@
+export const slideTextContentStyle = {
+	position: "absolute",
+	maxWidth: "350px",
+	display: "flex",
+	flexDirection: "column",
+	alignItems: "flex-start",
+	left: "170px",
+	top: "50px",
+};
+
+export const lightSlideTitle = {
+	color: "#FFF",
+};
+export const darkSlideTitle = {
+	color: "#000",
+};
+
+export const lightSlideDescription = {
+	padding: "20px 0",
+	fontSize: "30px",
+	color: "#FFF",
+};
+export const darkSlideDescription = {
+	padding: "20px 0",
+	fontSize: "30px",
+	color: "#000",
+};
+
+export const darkBackButton = {
+	width: "172px",
+	height: "44px",
+	background: "#FFF",
+};
+
+export const lightBackButton = {
+	width: "172px",
+	height: "44px",
+	background: "#007042",
+};
