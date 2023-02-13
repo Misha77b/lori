895c531d0bd436915927610131444fc186fa1c16
Misha <misha77b@gmail.com>
2023-02-13 17:09:31 +0200

    slider controllers pagination and change btns styling

--- client/src/components/Slider/Slider.jsx
+++ client/src/components/Slider/Slider.jsx
@@ -9,7 +9,7 @@ import "./Slider.scss";
 
 const Slider = () => {
 	return (
-		<Box sx={{ maxWidth: "1200px", maxHeight: "445px", margin: "0 auto" }}>
+		<Box sx={{ maxWidth: "1200px", margin: "0 auto" }}>
 			<Swiper
 				navigation={true}
 				slidesPerView={1}
@@ -20,26 +20,30 @@ const Slider = () => {
 				modules={[Navigation, Pagination]}
 				className="mySwiper"
 			>
-				<SwiperSlide>
+				<SwiperSlide className="swiper-item">
 					<img
+						className="swiper-slide-img"
 						style={{ width: "100%", height: "100%" }}
 						src="https://res.cloudinary.com/dsx708og4/image/upload/v1676222627/Lori_project/Slide1_iPhone-14-pro-max_paqdb6.jpg"
 					/>
 				</SwiperSlide>
-				<SwiperSlide>
+				<SwiperSlide className="swiper-item">
 					<img
+						className="swiper-slide-img"
 						style={{ width: "100%", height: "100%" }}
 						src="https://res.cloudinary.com/dsx708og4/image/upload/v1676222653/Lori_project/Slide2_Samsung-s23-ultra_nlgwjf.jpg"
 					/>
 				</SwiperSlide>
-				<SwiperSlide>
+				<SwiperSlide className="swiper-item">
 					<img
+						className="swiper-slide-img"
 						style={{ width: "100%", height: "100%" }}
 						src="https://res.cloudinary.com/dsx708og4/image/upload/v1676222787/Lori_project/slide3_Huawei-mate-50-series_whoidn.jpg"
 					/>
 				</SwiperSlide>
-				<SwiperSlide>
+				<SwiperSlide className="swiper-item">
 					<img
+						className="swiper-slide-img"
 						style={{ width: "100%", height: "100%" }}
 						src="https://res.cloudinary.com/dsx708og4/image/upload/v1676223534/Lori_project/Slide4_Xiaomi-11-lite_d23daa.jpg"
 					/>
