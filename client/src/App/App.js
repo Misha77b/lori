2ffb0e0b0ee8824b577c7c59fc8c3b467ee63caa
Yaroslav Prystupliuk <tyucha_05@ukr.net>
2023-02-13 17:10:57 +0200

    add header and and  started doing adaptability

--- client/src/App/App.js
+++ client/src/App/App.js
@@ -3,13 +3,15 @@ import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "@mui/material";
 import RootRouters from "../Router/Router";
 import theme from "../theming";
-// import Header from "../components/Header/Header";
+import Header from "../components/Header";
+
 // import Footer from "../components/Footer/Footer";
 
 const App = () => {
 	return (
 		<ThemeProvider theme={theme}>
 			<BrowserRouter>
+				<Header />
 				<RootRouters />
 			</BrowserRouter>
 		</ThemeProvider>
