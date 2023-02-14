47d05d25ea7dae4f37db49a20ad59cb7a78cf4bf
Yaroslav Prystupliuk <tyucha_05@ukr.net>
2023-02-14 15:18:50 +0200

    Add link in router

---
+++ client/src/pages/ExchangeAndReturn/ExchangeAndReturn.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ExchangeAndReturn = () => {
+	return <div>Exchange And Return</div>;
+};
+
+export default ExchangeAndReturn;
---
+++ client/src/pages/PaymentAndDelivery/PaymentAndDelivery.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PaymentAndDelivery = () => {
+	return <div>PaymentAndDelivery</div>;
+};
+
+export default PaymentAndDelivery;
