82b2c5d1b4cbe463be7678975b87aad46baef0cb
Ярослав Приступлюк <106585260+YaroslavPrystupluk@users.noreply.github.com>
2023-03-02 22:28:35 +0200

    add breadcrumbs

--- client/src/components/Header/components/Breadcrumbs/Breadcrumbs.jsx
+++ client/src/components/Header/components/Breadcrumbs/Breadcrumbs.jsx
@@ -1,27 +1,22 @@
 import React from "react";
-import { Breadcrumbs, Link, Box, Typography } from "@mui/material";
-import HomeIcon from "@mui/icons-material/Home";
-import useBreadcrumbs from "./hook";
+import useBreadcrumbs from "use-react-router-breadcrumbs";
+import { Breadcrumbs, Link } from "@mui/material";
+import { data } from "./helper/helperBreadcrump";
 
 const Breadcrumb = () => {
-	const routes = useBreadcrumbs();
-	console.log(routes);
+	const breadcrumbs = useBreadcrumbs(data);
+	console.log(breadcrumbs);
 
-	const links = routes.map((route, index) => (
-		<Typography key={index} color="inherit">
-			{route.name}
-		</Typography>
+	const links = breadcrumbs.map(({ breadcrumb, match }, index) => (
+		<span key={match.url}>
+			<Link underline="hover" color="grey.main" key={match.url} to={match.url}>
+				{breadcrumb}
+			</Link>
+			{index < breadcrumbs.length - 1}
+		</span>
 	));
 	return (
 		<Breadcrumbs mb={2} aria-label="breadcrumb">
-			<Link
-				underline="hover"
-				sx={{ display: "flex", alignItems: "center" }}
-				color="inherit"
-				href="/"
-			>
-				<HomeIcon sx={{ mr: 0.5 }} fontSize="inherit" />
-			</Link>
 			{links}
 		</Breadcrumbs>
 	);
---
+++ client/src/components/Header/components/Breadcrumbs/helper/helperBreadcrump.js
@@ -0,0 +1,14 @@
+import HomeIcon from "@mui/icons-material/Home";
+
+export const data = [
+	{ breadcrumb: HomeIcon, path: "/" },
+	{ breadcrumb: "Каталог", path: "/products" },
+	{ breadcrumb: "Оформлення заказу", path: "/orders" },
+	{ breadcrumb: "Про нас", path: "/about" },
+	{ breadcrumb: "Корзина", path: "/cart" },
+	{ breadcrumb: "Обрані товари", path: "/favorites" },
+	{ breadcrumb: "Гарантія", path: "/guarantee" },
+	{ breadcrumb: "Оплата і доставка", path: "/paymentAndDelivery" },
+	{ breadcrumb: "Обмін та повернення", path: "/exchangeAndReturn" },
+	{ breadcrumb: "Контакти", path: "/contacts" },
+];
