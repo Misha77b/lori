09a3d9a0b99723fdc333ad32221c8e5a51b4f85d
Yaroslav Prystupliuk <tyucha_05@ukr.net>
2023-03-06 20:38:20 +0200

    add

--- client/src/pages/Profile/ProfileMenuBlocks/OrdersHistory/OrdersHistory.jsx
+++ client/src/pages/Profile/ProfileMenuBlocks/OrdersHistory/OrdersHistory.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import { useSelector } from "react-redux";
+// import { selectHistoryOrders } from "../../../../store/selectors/orders.selectors";
 
 const OrdersHistory = () => {
-	return <div>Orders History</div>;
+	// const orders = useSelector(selectHistoryOrders);
+	// console.log(orders);
+
+	return (
+		<div>
+			<h2>Історія замовлень</h2>
+			{/* <ul>
+				{orders.length &&
+					orders?.map((order) => (
+						<li key={order.id}>
+							<div>Замовлення №: {order.orderNo}</div>
+							<div>Кількість: {order.quantity}</div>
+							<div>Дата: {order.date}</div>
+							<div>Сума: {order.totalSum}</div>
+							<div>Статус: {order.status}</div>
+						</li>
+					))}
+			</ul> */}
+		</div>
+	);
 };
 
 export default OrdersHistory;
