4a8cbc496595eaa9b13e1184c49a1a3fa46e532e
Yaroslav Prystupliuk <tyucha_05@ukr.net>
2023-03-07 03:42:29 +0200

    Authorization

--- client/src/pages/Profile/ProfileMenuBlocks/OrdersHistory/OrdersHistory.jsx
+++ client/src/pages/Profile/ProfileMenuBlocks/OrdersHistory/OrdersHistory.jsx
@@ -1,54 +1,40 @@
 import React, { useState, useEffect } from "react";
-import { useSelector } from "react-redux";
 import axios from "axios";
 import { DOMAIN } from "../../../../config/API";
-import {
-	selectAuth,
-	selectAuthUserToken,
-	selectUser,
-	selectUserID,
-} from "../../../../store/selectors/auth.selectors";
-// import { selectHistoryOrders } from "../../../../store/selectors/orders.selectors";
 
 const OrdersHistory = () => {
 	const [orders, setOrders] = useState([]);
-	const accessToken = useSelector(selectAuth);
-	// const { ...isAuth } = isLoggedIn;
-	console.log(accessToken);
+	// console.log(orders);
+	const accessToken = localStorage.getItem("token");
 	useEffect(() => {
-		// axios.get(`${DOMAIN}/customers/login`, {
-		// 	headers: {
-		// 		Authorization: `Bearer ${accessToken}`,
-		// 	},
-		// });
 		axios
 			.get(`${DOMAIN}/orders`, {
 				headers: {
-					Authorization: `Bearer ${accessToken}`,
+					Authorization: accessToken,
 				},
 			})
 			.then((response) => {
 				setOrders(response.data);
+				console.log(response.data);
 			})
 			.catch((error) => {
 				console.log(error);
 			});
-	}, [accessToken]);
+	}, []);
 
 	return (
 		<div>
 			<h2>Історія замовлень</h2>
 			<ul>
-				{accessToken &&
-					orders?.map((order) => (
-						<li key={order.id}>
-							<div>Замовлення №: {order.orderNo}</div>
-							<div>Кількість: {order.quantity}</div>
-							<div>Дата: {order.date}</div>
-							<div>Сума: {order.totalSum}</div>
-							<div>Статус: {order.status}</div>
-						</li>
-					))}
+				{orders?.map((order) => (
+					<li key={order.id}>
+						<div>Замовлення №: {order.orderNo}</div>
+						<div>Кількість: {order.quantity}</div>
+						<div>Дата: {order.date}</div>
+						<div>Сума: {order.totalSum}</div>
+						<div>Статус: {order.status}</div>
+					</li>
+				))}
 			</ul>
 		</div>
 	);
