13ac1931a6bede0427a249bfcaa611b638c18e49
Misha <misha77b@gmail.com>
2023-03-09 18:56:57 +0200

    privat route

---
+++ client/src/Router/ProtectedRoute/ProtectedRoute.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { useLocation, Navigate } from "react-router-dom";
+
+const ProtectedRoute = ({ children }) => {
+	const isAuth = useSelector((state) => state.auth.isAuth);
+	console.log(isAuth);
+
+	if (!isAuth) {
+		return <Navigate to="/" replace />;
+	}
+
+	return children;
+};
+
+export default ProtectedRoute;
