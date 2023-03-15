db3748eb1a9c060bc4e6a7650478fc2b068f15ef
Ярослав Приступлюк <106585260+YaroslavPrystupluk@users.noreply.github.com>
2023-03-15 16:33:44 +0200

    favoritrSlice add

---
+++ client/src/store/reducers/test/favoriteSlice.test.js
@@ -0,0 +1,35 @@
+import favoriteReducer, { setFavorite, removeItemFavorite } from "../favoriteSlice";
+
+const initialState = {
+	favorite: JSON.parse(localStorage.getItem("favorites")) || [],
+};
+
+const mockCard = {
+	name: "TouYinger M4 FullHD",
+	price: 3700,
+	imageUrls: "https://images.prom.ua/3468122255_touyinger-m4-fullhd.jpg",
+	article: 12345,
+	brand: "Apple",
+};
+
+describe("favoriteSlice", () => {
+	it("should return default state when passed an empty actions", () => {
+		const result = favoriteReducer(undefined, { type: "" });
+
+		expect(result).toEqual(initialState);
+	});
+
+	it("should add new goods to favorites 'setFavorite' action", () => {
+		const action = { type: setFavorite.type, payload: mockCard };
+		const result = favoriteReducer(initialState, action);
+
+		expect(result.favorite).toEqual([mockCard]);
+	});
+
+	it("should remove goods to favorites by id 'actionAddIsFavorite' action", () => {
+		const action = { type: removeItemFavorite.type, payload: mockCard.article };
+		const result = favoriteReducer(initialState, action);
+		expect(result.favorite.article).not.toEqual(mockCard.article);
+		expect(result.favorite).toEqual([]);
+	});
+});
