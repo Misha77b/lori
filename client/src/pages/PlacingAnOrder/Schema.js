c0e2a749043335c7849455cfef6bcdeafab72871
Misha <misha77b@gmail.com>
2023-03-17 16:45:17 +0200

    email validation, number to profile info

--- client/src/pages/PlacingAnOrder/Schema.js
+++ client/src/pages/PlacingAnOrder/Schema.js
@@ -4,6 +4,6 @@ const REQUIRED = "This field is required to fill in";
 
 export const schema = yup.object().shape({
 	fullName: yup.string().required(REQUIRED),
-	email: yup.string("Enter your email").required("Email is required"),
+	email: yup.string("Enter your email").email("Enter a valid email").required("Email is required"),
 	phoneNumber: yup.number().required(REQUIRED),
 });
