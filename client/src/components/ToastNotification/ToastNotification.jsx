bf046fc16619366c2127f4cc5708b444621ff697
Vitalina Porada <porada.vita@icloud.com>
2023-03-17 13:17:56 +0200

    fixed my bugs from Trello

--- client/src/components/ToastNotification/ToastNotification.jsx
+++ client/src/components/ToastNotification/ToastNotification.jsx
@@ -15,12 +15,12 @@ const ToastWrapper = styled.div`
 	display: block;
 	text-align: end;
 	max-width: 250px;
+	position: fixed;
+	border-radius: 10px;
 	margin: 0 auto 20px auto;
 	padding: 25px 15px;
-	position: absolute;
 	right: 2%;
 	z-index: 5;
-	border-radius: 10px;
 	box-shadow: 0 1px 1px rgba(0, 0, 0, 0.1), inset 0 1px 0 rgba(255, 255, 255, 0.6);
 `;
 const Message = styled.p`
