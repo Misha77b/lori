862eee795aab39d8efe3a9911bc93c36843f0896
Inna <kurylo.salamonova@gmail.com>
2023-03-20 11:05:44 +0200

    filter.js has change

--- controllers/filters.js
+++ controllers/filters.js
@@ -1,38 +1,71 @@
 const Filter = require("../models/Filter");
+const Product = require("../models/Product");
 const queryCreator = require("../commonHelpers/queryCreator");
 const _ = require("lodash");
 
+const group = {
+  $group: {
+    _id: null,
+    minPrice: {
+      $min: "$currentPrice",
+    },
+    maxPrice: {
+      $max: "$currentPrice",
+    },
+    brand: {
+      $addToSet: "$brand",
+    },
+    screenType: {
+      $addToSet: "$screenType",
+    },
+    diagonal: {
+      $addToSet: "$diagonal",
+    },
+    RAM: {
+      $addToSet: "$RAM",
+    },
+    iternalStorage: {
+      $addToSet: "$iternalStorage",
+    },
+    processor: {
+      $addToSet: "$processor",
+    },
+    waterResistant: {
+      $addToSet: "$waterResistant",
+    },
+  },
+};
 exports.addFilter = (req, res, next) => {
-  Filter.findOne({ name: req.body.name, type: req.body.type }).then(filter => {
-    if (filter) {
-      return res.status(400).json({
-        message: `Filter with type "${filter.type}" and name "${filter.name}" already exists`
-      });
-    } else {
-      const initialQuery = _.cloneDeep(req.body);
-      const newFilter = new Filter(queryCreator(initialQuery));
+  Filter.findOne({ name: req.body.name, type: req.body.type }).then(
+    (filter) => {
+      if (filter) {
+        return res.status(400).json({
+          message: `Filter with type "${filter.type}" and name "${filter.name}" already exists`,
+        });
+      } else {
+        const initialQuery = _.cloneDeep(req.body);
+        const newFilter = new Filter(queryCreator(initialQuery));
 
-      newFilter
-        .save()
-        .then(filter => res.json(filter))
-        .catch(err =>
-          res.status(400).json({
-            message: `Error happened on server: "${err}" `
-          })
-        );
+        newFilter
+          .save()
+          .then((filter) => res.json(filter))
+          .catch((err) =>
+            res.status(400).json({
+              message: `Error happened on server: "${err}" `,
+            })
+          );
+      }
     }
-  });
+  );
 };
 
 exports.updateFilter = (req, res, next) => {
   Filter.findOne({ _id: req.params.id })
-    .then(filter => {
+    .then((filter) => {
       if (!filter) {
-        return res
-          .status(400)
-          .json({
-            message: `Filter with _id "${req.params.id}" is not found.`
-          });
+        return res.status(400).json({
+          message: `Filter with _id "${req.params.id}" is not found.`,
+        });
       } else {
         const initialQuery = _.cloneDeep(req.body);
         const updatedFilter = queryCreator(initialQuery);
@@ -42,23 +75,23 @@ exports.updateFilter = (req, res, next) => {
           { $set: updatedFilter },
           { new: true }
         )
-          .then(filter => res.json(filter))
-          .catch(err =>
+          .then((filter) => res.json(filter))
+          .catch((err) =>
             res.status(400).json({
-              message: `Error happened on server: "${err}" `
+              message: `Error happened on server: "${err}" `,
             })
           );
       }
     })
-    .catch(err =>
+    .catch((err) =>
       res.status(400).json({
-        message: `Error happened on server: "${err}" `
+        message: `Error happened on server: "${err}" `,
       })
     );
 };
 
 exports.deleteFilter = (req, res, next) => {
-  Filter.findOne({ _id: req.params.id }).then(async filter => {
+  Filter.findOne({ _id: req.params.id }).then(async (filter) => {
     if (!filter) {
       return res
         .status(400)
@@ -67,14 +100,14 @@ exports.deleteFilter = (req, res, next) => {
       const filterToDelete = await Filter.findOne({ _id: req.params.id });
 
       Filter.deleteOne({ _id: req.params.id })
-        .then(deletedCount =>
+        .then((deletedCount) =>
           res.status(200).json({
-            message: `Filter witn type "${filterToDelete.type}" and name "${filterToDelete.name}" is successfully deletes from DB `
+            message: `Filter witn type "${filterToDelete.type}" and name "${filterToDelete.name}" is successfully deletes from DB `,
           })
         )
-        .catch(err =>
+        .catch((err) =>
           res.status(400).json({
-            message: `Error happened on server: "${err}" `
+            message: `Error happened on server: "${err}" `,
           })
         );
     }
@@ -83,20 +116,69 @@ exports.deleteFilter = (req, res, next) => {
 
 exports.getFilters = (req, res, next) => {
   Filter.find()
-    .then(filters => res.json(filters))
-    .catch(err =>
+    .then((filters) => res.json(filters))
+    .catch((err) =>
       res.status(400).json({
-        message: `Error happened on server: "${err}" `
+        message: `Error happened on server: "${err}" `,
       })
     );
 };
 
 exports.getFiltersByType = (req, res, next) => {
-  Filter.find({ type: req.params.type })
-    .then(filters => res.json(filters))
-    .catch(err =>
-      res.status(400).json({
-        message: `Error happened on server: "${err}" `
-      })
-    );
+  if (req.params.type == "agregate") {
+    const query = [];
+    const fieldQuery = Object.keys(req.query);
+    if (fieldQuery.length) {
+      const match = {};
+      fieldQuery.forEach((field) => {
+        switch (field) {
+          case "minPrice":
+            // {
+            //   let filterPrice = {};
+            //   if (match.currentPrice) {
+            //     filterPrice = match.currentPrice;
+            //   }
+            //   filterPrice["$gte"] = +req.query[field];
+            //   match.currentPrice = filterPrice;
+            // }
+
+            break;
+          case "maxPrice":
+            // {
+            //   let filterPrice = {};
+            //   if (match.currentPrice) {
+            //     filterPrice = match.currentPrice;
+            //   }
+            //   filterPrice["$lte"] = +req.query[field];
+            //   match.currentPrice = filterPrice;
+            // }
+            break;
+          case "sort":
+            break;
+          default:
+            match[field] = req.query[field];
+            break;
+        }
+      });
+      query.push({ $match: match });
+    }
+
+    query.push(group);
+
+    Product.aggregate(query)
+      .then((filters) => res.json(filters))
+      .catch((err) =>
+        res.status(400).json({
+          message: `Error happened on server: "${err}" `,
+        })
+      );
+  } else {
+    Filter.find({ type: req.params.type })
+      .then((filters) => res.json(filters))
+      .catch((err) =>
+        res.status(400).json({
+          message: `Error happened on server: "${err}" `,
+        })
+      );
+  }
 };
