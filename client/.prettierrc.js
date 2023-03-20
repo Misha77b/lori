b01995ca7f37a340b9b07a32d7879716dffd0fcd
Misha <misha77b@gmail.com>
2023-03-20 15:59:40 +0200

    eslint tab width testing

--- client/.prettierrc.js
+++ client/.prettierrc.js
@@ -5,5 +5,15 @@ module.exports = {
 	semi: true,
 	trailingComma: "all",
 	singleQuote: false,
+	overrides: [
+		{
+			files: "**/lwc/**/*.html",
+			options: { parser: "lwc" },
+		},
+		{
+			files: "*.{cmp,page,component}",
+			options: { parser: "html" },
+		},
+	],
 	// singleAttributePerLine: true,
 };
